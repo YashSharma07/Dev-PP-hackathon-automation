36b7c7cf2087457b7362dfd51d0b9e2d46d8add5
Yash Sharma <YASHQWER56@GMAIL.COM>
2021-04-11 14:20:42 +0530

    Add files via upload

---
+++ automate.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+let tab;
+let search = "mouse";
+
+
+(async function(){
+
+    //opens the browser
+    let browser = await puppeteer.launch({ headless : false , defaultViewport: null, args : ["--start-maximized"]});
+
+    // get all pages and then choose the first tab
+    let allpages = await browser.pages();
+    tab = allpages[0];
+
+    // goto the URL
+    await tab.goto("https://amazon.in");
+
+    // search for the element
+    await tab.type("#twotabsearchtextbox",search);
+    await tab.click("#nav-search-submit-button");
+
+    // access and click 4 star and above rating
+    await tab.waitForSelector(".a-star-medium-4", {visible : true})
+    await tab.click(".a-star-medium-4");
+
+    // choose the range
+    await tab.waitForSelector(".a-star-medium-4", {visible : true})
+    await tab.click(".a-star-medium-4");
+});
+
+
+console.log("Code will start soon");
